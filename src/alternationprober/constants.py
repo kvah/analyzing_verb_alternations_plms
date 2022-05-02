de3e1f5107576f7a56637415cb1d0a7120a8e4cd
David Yi <davidyi624@berkeley.edu>
2022-05-01 23:53:34 -0700

    Add script for generating contextual verb embeddings

--- src/alternationprober/constants.py
+++ src/alternationprober/constants.py
@@ -17,11 +17,12 @@ _THIS_DIR = Path(__file__).resolve().parent
 _DATA_DIR = _THIS_DIR.parents[1] / "data"
 
 PATH_TO_LAVA_FILE = _DATA_DIR / "lava" / "all_verbs.csv"
+PATH_TO_FAVA_DIR = _DATA_DIR / "fava"
 PATH_TO_BERT_WORD_EMBEDDINGS_FILE = (
     _DATA_DIR / "embeddings" / "bert-word-embeddings-lava.npy"
 )
-PATH_TO_BERT_WORD_CONTEXT_EMBEDDINGS_FILE = (
-    _DATA_DIR / "embeddings" / "bert-word-context-embeddings.npy"
+PATH_TO_BERT_WORD_CONTEXT_EMBEDDINGS_DIR = (
+    _DATA_DIR / "embeddings" 
 )
 
 PATH_TO_LAVA_VOCAB = _DATA_DIR / "embeddings" / "bert-word-embeddings-lava-vocab.json"
